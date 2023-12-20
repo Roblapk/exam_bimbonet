47e9969775d6685d003684b8d23cf1323b9afa57
Rogelio Bernal <ing.rogelio.bernal@gmail.com>
2023-12-20 14:15:15 -0600

    initial commit project configuration

---
+++ exam_bimboNet/exam_bimboNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  exam_bimboNetApp.swift
+//  exam_bimboNet
+//
+//  Created by Rogelio B on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct exam_bimboNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
